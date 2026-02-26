0842e2f450d42f586fc87d05fdee0c6ba0bd0549
metalomana <metalomana@gmail.com>
2026-02-26 19:47:37 +0300

    Create Dog.java

---
+++ Work_1/Dog.java
@@ -0,0 +1,34 @@
+package Work_1;
+
+public class Dog extends Animal { private static int dogCount = 0;
+
+    private static final int MAX_RUN = 500;
+    private static final int MAX_SWIM = 10;
+
+    public Dog(String name) {
+        super(name);
+        dogCount++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance <= MAX_RUN) {
+            System.out.println(name + " пробежал " + distance + " м.");
+        } else {
+            System.out.println(name + " не может пробежать " + distance + " м.");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance <= MAX_SWIM) {
+            System.out.println(name + " проплыл " + distance + " м.");
+        } else {
+            System.out.println(name + " не может проплыть " + distance + " м.");
+        }
+    }
+
+    public static int getDogCount() {
+        return dogCount;
+    }
+}
