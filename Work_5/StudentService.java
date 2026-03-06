eb15cc21fa7437ef469f048a9614b93287315bfb
metalomana <metalomana@gmail.com>
2026-03-06 10:45:39 +0300

    Create StudentService.java

---
+++ Work_5/StudentService.java
@@ -0,0 +1,34 @@
+package Work_5;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class StudentService {
+
+    public static void removeLowAverageStudents(Set<Student> students) {
+        Iterator<Student> iterator = students.iterator();
+
+        while (iterator.hasNext()) {
+            Student student = iterator.next();
+            if (student.getAverageGrade() < 3.0) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void promoteStudents(Set<Student> students) {
+        for (Student student : students) {
+            if (student.getAverageGrade() >= 3.0) {
+                student.setCourse(student.getCourse() + 1);
+            }
+        }
+    }
+
+    public static void printStudents(Set<Student> students, int course) {
+        for (Student student : students) {
+            if (student.getCourse() == course) {
+                System.out.println(student.getName());
+            }
+        }
+    }
+}
