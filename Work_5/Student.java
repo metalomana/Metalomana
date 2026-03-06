5fbd0a73014da06fd2aed6f59468077fb1a0e487
metalomana <metalomana@gmail.com>
2026-03-06 10:44:53 +0300

    Create Student.java

---
+++ Work_5/Student.java
@@ -0,0 +1,70 @@
+package Work_5;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Student {
+
+    private String name;
+    private String group;
+    private int course;
+    private List<Integer> grades;
+
+    public Student(String name, String group, int course, List<Integer> grades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getAverageGrade() {
+        return grades.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", group='" + group + '\'' +
+                ", course=" + course +
+                ", average=" + getAverageGrade() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) &&
+                Objects.equals(group, student.group);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, group);
+    }
+}
